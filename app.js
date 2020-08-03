0886cd971ba0debfe52d5cfc1d2fcd1558c4c977
ddoniddoni <psdkei@naver.com>
2020-08-03 17:55:51 +0900

    Add Pug

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
-app.use(cookieParser());
-app.use(bodyParser.json());
+app.set("view engine", "pug");
+app.use(cookieParser()); // 사용자 인증 같은 곳에서 쿠키 검사할 때
+app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보 검사 미들웨어
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(morgan("dev"));
+app.use(helmet()); // 안전하도록 만드는 미들웨어
+app.use(morgan("dev")); // 발생하는 모든일들을 logging하는 미들웨어
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
