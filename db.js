0b0a2da0c329aece1027bff89834bddf8ed9c1b0
ddoniddoni <psdkei@naver.com>
2020-08-07 04:19:10 +0900

    Update Fake DB and output Home

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "멋진 동영상",
+    description: "테스트 동영상",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 60645,
+      name: "박상돈",
+      email: "psdkei@naver.com",
+    },
+  },
+  {
+    id: 1561567,
+    title: "멋진 동영상2",
+    description: "테스트 동영상",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 60645,
+      name: "박상돈",
+      email: "psdkei@naver.com",
+    },
+  },
+  {
+    id: 98765,
+    title: "멋진 동영상3",
+    description: "테스트 동영상",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 60645,
+      name: "박상돈",
+      email: "psdkei@naver.com",
+    },
+  },
+  {
+    id: 332497,
+    title: "멋진 동영상4",
+    description: "테스트 동영상",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 60645,
+      name: "박상돈",
+      email: "psdkei@naver.com",
+    },
+  },
+];
